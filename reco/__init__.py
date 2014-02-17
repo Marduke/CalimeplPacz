b20bad1dfcf28aee1add58a793700605b103ac49
MarDuke <marduke@centrum.cz>
2014-02-17 22:26:37 +0100

    reco update

--- reco/__init__.py
+++ reco/__init__.py
@@ -7,12 +7,21 @@ __docformat__ = 'restructuredtext en'
 
 import os, sqlite3, re
 
-def addDir(stack, directory):
-    for n in os.listdir(directory):
-        stack.append(directory + n)
 
-def a(arr):
-    print(arr)
+def check(title, authors):
+    '''
+    Check nkp
+    '''
+    pass
+
+def make_combo(tokens):
+    pass
+
+def check_combo(arr):
+    pass
+#     my db
+#     cal db
+#     nkp
 
 if __name__ == '__main__':
     conn = sqlite3.connect('reco.db')
@@ -21,13 +30,11 @@ if __name__ == '__main__':
         (id INT PRIMARY KEY     NOT NULL,
         title    TEXT    NOT NULL,
         authors    INT     NOT NULL);''')
-    conn.execute('''CREATE TABLE IF NOT EXISTS file
-           (id INT PRIMARY KEY    NOT NULL,
-           org_path    TEXT    NOT NULL,
-           hash    INT    NOT NULL,
-           id_book    INT);''')
+    conn.execute('''CREATE TABLE IF NOT EXISTS author
+        (id INT PRIMARY KEY     NOT NULL,
+        name    TEXT     NOT NULL);''')
 
-#     conn = sqlite3.connect('\\data\\knihovna\\Calibre Library\\metadata.db')
+    conn_cal = sqlite3.connect('\\data\\knihovna\\Calibre Library\\metadata.db')
 
 #     cursor = conn.execute("SELECT * FROM sqlite_master WHERE type='table'")
 #     for row in cursor:
@@ -38,33 +45,30 @@ if __name__ == '__main__':
 #         print([row[1], row[6]])
 #
 #     conn.close()
-    regexps = {
-               "(.*)-(.*)\.txt":a
-    }
-    files = [".txt"]
+    files = [".txt", ".docx", ".doc", ".rtf", ".epub", ".pdb", ".djvu", ".mobi"]
 
-#     directory = u"e:\\private\\tr\\mdone\\Knihy Sbírka (Fantasy Sci-Fi)\\Knihy\\I\\IRVING David\\"
-    directory = u"e:\\private\\tr\\mdone\\Knihy Sbírka (Fantasy Sci-Fi)\\Knihy\\I\\IRVING David\\"
-    for f in os.listdir(directory):
-        if not f[-4:] in files:
-            continue
-        for reg in regexps.iteritems():
+    dirs = []
+#     dirs.append("E:\\data\\knihy na prenos\\ebook\\")
+    dirs.append("e:\\data\\knihy na prenos\\ebook\\E-Knihy.mobi\\Asimov, Isaac\\")
+
+    try:
+        directory = dirs.pop()
+        for f in os.listdir(directory):
+            act = directory + f
             print(f)
-            print(reg)
-            if re.match(reg[0], f):
-                match = re.search(reg[0], f)
-                reg[1](match.groups())
+            if os.path.isdir(act):
+                dirs.append(act)
+            else:
+                pos = f.rfind(".")
+                if not f[pos:] in files:
+                    print("pripona ee")
+                    continue
+                name = f[:pos]
+                tokens = re.findall(r"[\w',]+", name)
+                combo = make_combo(tokens)
+                result = check_combo(combo)
+    except Exception as e:
+        print(e)
 
-#     directory = "E:\\private\\tr\\mdone\\"
-#     acceptable = ["txt"]
-#     stack = []
-#     addDir(stack, directory)
-#
-#     while(len(stack) > 0):
-#         thi = stack.pop()
-#         print thi
-#         if os.path.isdir(thi):
-#             print "dir"
-#             addDir(stack, thi)
-#         else:
-#             print "file"
+    conn.close()
+    conn_cal.close()
